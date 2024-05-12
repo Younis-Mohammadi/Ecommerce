08381a4e1bf02eb8d86937c59d6603939a59d70e
Younis-Mohammadi <artacodding2023@gmail.com>
2024-05-12 08:42:24 +0430

    admin panel created

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
     <meta name="author" content="NobleUI">
-    <meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <meta name="keywords"
+        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
     <title>Login Page</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,12 +19,14 @@
     <link rel="stylesheet" href="{{asset('admin_assets/css/demo2/style.css')}}">
     <link rel="shortcut icon" href="{{asset('admin_assets/images/favicon.png')}}" />
 </head>
+
 <body>
     <div class="main-wrapper">
         <div class="page-wrapper full-page">
             <div class="page-content d-flex align-items-center justify-content-center">
                 <div class="row w-100 mx-0 auth-page">
                     <div class="col-md-8 col-xl-6 mx-auto">
+                        @include('admin.message')
                         <div class="card">
                             <div class="row">
                                 <div class="col-md-4 pe-md-0">
@@ -32,35 +36,43 @@
                                 <div class="col-md-8 ps-md-0">
                                     <div class="auth-form-wrapper px-4 py-5">
                                         <a href="#"
-                                            class="noble-ui-logo logo-light d-block mb-2">Noble<span>UI</span></a>
-                                        <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
-                                        <form class="forms-sample">
+                                            class="noble-ui-logo logo-light d-block mb-2">Top<span>Dev</span></a>
+                                        <h5 class="text-white fw-normal mb-4">Welcome back! Log in to your account.</h5>
+                                        <form method="post" action="{{route('admin.authenticate')}}"
+                                            class="forms-sample">
+                                            @csrf
                                             <div class="mb-3">
-                                                <label for="userEmail" class="form-label">Email address</label>
-                                                <input type="email" class="form-control" id="userEmail"
-                                                    placeholder="Email">
+                                                <label for="userEmail" class="form-label text-white">Email
+                                                    address</label>
+                                                <input type="email" name="email" id="email"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    placeholder="Email" value="{{ old('email') }}">
+                                                @error('email')
+                                                    <p class="invalid-feedback">{{$message}}</p>
+                                                @enderror
                                             </div>
+
                                             <div class="mb-3">
-                                                <label for="userPassword" class="form-label">Password</label>
-                                                <input type="password" class="form-control" id="userPassword"
-                                                    autocomplete="current-password" placeholder="Password">
+                                                <label for="userPassword" class="form-label text-white">Password</label>
+                                                <input type="password" name="password" id="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    autocomplete="current-password" placeholder="Password"
+                                                    value="{{ old('password') }}">
+                                                @error('password')
+                                                    <p class="invalid-feedback">{{$message}}</p>
+                                                @enderror
                                             </div>
                                             <div class="form-check mb-3">
                                                 <input type="checkbox" class="form-check-input" id="authCheck">
-                                                <label class="form-check-label" for="authCheck">
+                                                <label class="form-check-label text-white" for="authCheck">
                                                     Remember me
                                                 </label>
                                             </div>
                                             <div>
-                                                <a href="../../dashboard.html"
-                                                    class="btn btn-primary me-2 mb-2 mb-md-0 text-white">Login</a>
-                                                <button type="button"
-                                                    class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
-                                                    <i class="btn-icon-prepend" data-feather="twitter"></i>
-                                                    Login with twitter
-                                                </button>
+                                                <button type="submit"
+                                                    class="btn btn-primary me-2 mb-2 mb-md-0 text-white">submit</button>
                                             </div>
-                                            <a href="register.html" class="d-block mt-3 text-muted">Not a user? Sign
+                                            <a href="register.html" class="d-block mt-3 text-white">Not a user? Sign
                                                 up</a>
                                         </form>
                                     </div>
@@ -72,8 +84,10 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{asset('admin_assets/vendors/core/core.js')}}"></script>
     <script src="{{asset('admin_assets/vendors/feather-icons/feather.min.js')}}"></script>
     <script src="{{asset('admin_assets/js/template.js')}}"></script>
 </body>
+
 </html>
